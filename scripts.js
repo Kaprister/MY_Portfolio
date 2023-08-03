767284ab0869838df2f51aeea7170e6f7b62db53
Kaprister <sushantkapri@gamil.com>
2023-08-03 13:21:04 +0530

    update script.js

--- scripts.js
+++ scripts.js
@@ -21,11 +21,21 @@ btn.addEventListener("click", function(event){
     templateParams.message = u_message.value;
     templateParams.email = u_email.value;
     templateParams.contact = u_contact.value;
+
+    if(!templateParams.name || !templateParams.email || !templateParams.contact || !templateParams.message){
+        window.alert("Please fill all the details!");
+        return;
+    }
     console.log(templateParams);
+
     emailjs.send('service_yr6cw13', 'template_rkdjk69', templateParams, 'KtAa7BWE9p1xRgKle')
     .then(function(response) {
-    console.log('SUCCESS!', response.status, response.text);
-    }, function(error) {
-    console.log('FAILED...', error);
+        window.alert("Message sent successfully!");
+        console.log('SUCCESS!', response.status, response.text);
+        location.reload();
+    },function(error) {
+        window.alert("Something went wrong! please try again.");
+        console.log('FAILED...', error);
+        location.reload();
     });
 })
